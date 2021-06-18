1d4a4b7f4922d0af97c92718e6f72b7a9e9af19a
Jay Nabonne <jaynabonne@gmail.com>
2021-06-18 13:04:48 +0100

    DEO-1906: Moved files to folders.

--- test/auditing/EngagementAuditingSpec.scala
+++ test/auditing/EngagementAuditingSpec.scala
@@ -14,20 +14,38 @@
  * limitations under the License.
  */
 
+package auditing
+
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import mappers.EngagementMapper
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.{verify, when}
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.mockito.MockitoSugar
-import play.api.libs.json.{JsArray, JsObject, JsValue, Json}
+import play.api.libs.json.{JsArray, Json}
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.audit.model.ExtendedDataEvent
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class EngagementAuditingSpec extends AnyWordSpec with Matchers with MockitoSugar {
-  "EngagementAuditing" must {
+  "auditing.EngagementAuditing" must {
     "send audit events for engagement" in {
       val metadataEvent = ExtendedDataEvent("metadata", "Metadata")
       val transcriptEvent1 = ExtendedDataEvent("transcript1", "Transcript1")
