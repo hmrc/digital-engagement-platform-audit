1d91c6a3a29febf80eb7f391bfd02f25f33f813b
pratikrana06 <63359242+pratikrana06@users.noreply.github.com>
2022-03-28 11:19:25 +0100

    [DEP-2403][PR]:Dependencies and Library upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,20 +4,27 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.16.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.56.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.21.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.62.0"
   )
 
+  val akkaVersion = "2.6.19"
+
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.16.0",
-    "org.scalatest"           %% "scalatest"                % "3.2.10",
-    "org.scalatestplus"       %% "scalatestplus-mockito"    % "1.0.0-M2",
-    "org.mockito"             %  "mockito-core"             % "4.0.0",
-    "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.27.2",
-    "com.typesafe.play"       %% "play-test"                % PlayVersion.current,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.62.2",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0",
-    "com.typesafe.akka"       %% "akka-stream-testkit"      % "2.6.14",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.56.0"
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.21.0",
+    "org.scalatest"           %% "scalatest"                  % "3.2.11",
+    "org.scalatestplus"       %% "scalatestplus-mockito"      % "1.0.0-M2",
+    "org.mockito"             %  "mockito-core"               % "4.4.0",
+    "com.github.tomakehurst"  %  "wiremock-standalone"        % "2.27.2",
+    "com.typesafe.play"       %% "play-test"                  % PlayVersion.current,
+    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.62.2",
+    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0",
+    "com.typesafe.akka"       %% "akka-testkit"               % akkaVersion,
+    "com.typesafe.akka"       %% "akka-actor-typed"           % akkaVersion,
+    "com.typesafe.akka"       %% "akka-slf4j"                 % akkaVersion,
+    "com.typesafe.akka"       %% "akka-serialization-jackson" % akkaVersion,
+    "com.typesafe.akka"       %% "akka-protobuf-v3"           % akkaVersion,
+    "com.typesafe.akka"       %% "akka-stream"                % akkaVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.62.0"
   ).map(_ % "test, it")
 }
