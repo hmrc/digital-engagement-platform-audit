a34a0a05899252e7f1df42791f79e63faa8ccd87
AdamSmithHMRC <72032501+AdamSmithHMRC@users.noreply.github.com>
2024-03-05 15:39:47 +0000

    DEP-6096 play migration (#60)
    * DEP-6096 play migration
    
    * DEP-6096 WIP
    
    * DEP-6096 WIP
    
    * DEP-6096 WIP
    
    * DEP-6096 pr bot comment addressed

--- app/repositories/AuditJobRepository.scala
+++ app/repositories/AuditJobRepository.scala
@@ -19,6 +19,7 @@ package repositories
 import java.time.LocalDateTime
 
 import javax.inject.Inject
+import javax.inject.Singleton
 import models.AuditJob
 import org.mongodb.scala.model.Filters._
 import org.mongodb.scala.model.Updates._
@@ -30,6 +31,7 @@ import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
 
 import scala.concurrent.{ExecutionContext, Future}
 
+@Singleton
 class AuditJobRepository @Inject()(mongo: MongoComponent)(implicit ec: ExecutionContext
 ) extends PlayMongoRepository[AuditJob] (
   mongoComponent = mongo,
