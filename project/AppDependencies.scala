5223194692922b6f64d1275c6286b3dccaf3f4dd
jacksgannon <19840977+jacksgannon@users.noreply.github.com>
2025-04-09 17:41:07 +0100

    DEP-6404 updating dependency versions to latest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,8 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % "9.0.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % "2.0.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % "9.11.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % "2.6.0",
     "io.jsonwebtoken"         % "jjwt-api"                    % "0.11.5",
     "commons-codec"           %  "commons-codec"              % "1.15",
     "com.github.jwt-scala"    %% "jwt-core"                   % "9.1.2"
@@ -15,7 +15,7 @@ object AppDependencies {
   val PekkoHttpVersion = "1.0.1"
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % "9.0.0",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % "9.11.0",
     "org.scalatest"           %% "scalatest"                  % "3.2.12",
     "org.scalatestplus"       %% "scalatestplus-mockito"      % "1.0.0-M2",
     "org.mockito"             %  "mockito-core"               % "4.5.1",
@@ -24,6 +24,6 @@ object AppDependencies {
     "org.apache.pekko"        %% "pekko-actor-typed"          % PekkoVersion,
     "org.apache.pekko"        %% "pekko-serialization-jackson"% PekkoVersion,
     "org.apache.pekko"        %% "pekko-stream"               % PekkoVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % "2.0.0"
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % "2.6.0"
   ).map(_ % "test, it")
 }
